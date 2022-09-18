1ebab0606caa7076e16ace7413c731df01df21de
Tamaarine <irebootplaygt@gmail.com>
2022-09-18 01:49:07 +0000

    Finished implementing the rb-tree of the integers being inserted, included my_rb_insert. Printed out the rb-tree after it is done being inserted, and erased from the tree and free each of the nodes deleted

--- HW2/kds.c
+++ HW2/kds.c
@@ -4,6 +4,7 @@
 #include <linux/string.h>
 #include <linux/slab.h>
 #include <linux/list.h>
+#include <linux/rbtree.h>
 
 #define DRIVER_AUTHOR "Ricky Lu ricky.lu@stonybrook.edu"
 #define DRIVER_DESC   "CSE 306 Testing Module"
@@ -19,6 +20,40 @@ struct my_link_list_struct {
     struct list_head list; /* Kernel's embedded linked list struct use this to traverse */
 };
 
+/* mystruct for red-black tree */
+struct my_rb_tree_struct {
+    int data; /* Stores integer */
+    struct rb_node node; /* Kernel's embedded rb_node struct */
+};
+
+void my_rb_insert(struct rb_root * root, struct my_rb_tree_struct * new) {
+    /* Pointer to a pointer of rb_node used to traverse the tree */
+    struct rb_node ** link = &root->rb_node;
+    struct rb_node * parent = NULL; /* Used to store the last node visited */
+    int value = new->data; /* The value that we are inserting */
+    struct my_rb_tree_struct * current_struct; /* Used to store the entry struct */
+    
+    /* Descend down the tree until the place to insert is found, link == NULL */
+    while (*link) {
+        parent = *link; /* Store the node pointer that link is pointing to. The current node basically */
+        
+        /* Use rb_entry to find the encapsulating struct */
+        current_struct = rb_entry(parent, struct my_rb_tree_struct, node);
+        
+        /* make link point to left sub-tree if value is less than current node */
+        if (current_struct->data > value) 
+            link = &(*link)->rb_left;
+        else
+            link = &(*link)->rb_right;
+    }
+    
+    /* Insert the new node at parent */
+    rb_link_node(&new->node, parent, link);
+    
+    /* Rebalance if needed */
+    rb_insert_color(&new->node, root);
+}
+
 static int __init mymodule_init(void) {
     char * temp;
     char current_char;
@@ -28,7 +63,12 @@ static int __init mymodule_init(void) {
     /* Declaration for link list */
     LIST_HEAD(mylinkedlist); /* macro that defines the sentinel node (The head) */
     struct my_link_list_struct * position, * next; /* Temp variable used in iteration */
-    struct my_link_list_struct * to_add; /* Variable used to add to list */
+    struct my_link_list_struct * to_add_link_list; /* Variable used to add to list */
+    
+    /* Declaration for red-black tree */
+    struct rb_root mytree = RB_ROOT; /* Root node of the rb-tree, contains the rb_node pointer */
+    struct my_rb_tree_struct * to_add_rb_node; /* Variable used to add to rb-tree */
+    struct rb_node * rb_node_position, * rb_node_temp; /* Used for iteration */
 
     printk(KERN_INFO "My module entered!\n");
     
@@ -59,22 +99,33 @@ static int __init mymodule_init(void) {
                 printk(KERN_INFO "Number from insmod: %d\n", (int)parsed_res);
                 
                 /* Insert into the corresponding data structure here */
-                /* First linked list */
-                to_add = kmalloc(sizeof(struct my_link_list_struct), GFP_KERNEL);
-                
+                /* First, linked list */
+                to_add_link_list = kmalloc(sizeof(struct my_link_list_struct), GFP_KERNEL);
                 /* Memory allocation failed */
-                if (!to_add) {
+                if (!to_add_link_list) {
                     return -EINVAL;
                 }
                 
                 /* Add integer to the struct */
-                to_add->data = parsed_res;
-                INIT_LIST_HEAD(&to_add->list); /* set the node to point to itself first */
+                to_add_link_list->data = parsed_res;
+                INIT_LIST_HEAD(&to_add_link_list->list); /* set the node to point to itself first */
                 
                 /* Then we add it to the head */
-                list_add(&to_add->list, &mylinkedlist);
+                list_add(&to_add_link_list->list, &mylinkedlist);
                 
-                /*  */
+                
+                /* Second, red-black tree */
+                to_add_rb_node = kmalloc(sizeof(struct my_rb_tree_struct), GFP_KERNEL);
+                /* Memory allocation failed */
+                if (!to_add_rb_node) {
+                    return -EINVAL;
+                }
+                
+                /* Set the data of the inserted node */
+                to_add_rb_node->data = parsed_res;
+                to_add_rb_node->node.rb_left = NULL;
+                to_add_rb_node->node.rb_right = NULL;
+                my_rb_insert(&mytree, to_add_rb_node); /* Insert it by calling mb_rb_insert */
             }
         }
         
@@ -109,7 +160,22 @@ static int __init mymodule_init(void) {
         kfree(position);
     }
     
-    
+    /* Print out the integers inside the rb_tree then free */
+    rb_node_position = rb_first(&mytree);
+    /* While rb_first is not NULL, print then remove and free */
+    while (rb_node_position) {
+        /* Reuse to_add_rb_node for storing the struct pointer */
+        to_add_rb_node = rb_entry(rb_node_position, struct my_rb_tree_struct, node);
+        
+        printk(KERN_INFO "rb-tree data: %d\n", to_add_rb_node->data);
+        
+        rb_node_temp = rb_next(rb_node_position); /* Get next entry */
+        
+        rb_erase(rb_node_position, &mytree); /* Delete the node that was printed */
+        kfree(rb_node_position); /* Free the node that was deleted */
+        
+        rb_node_position = rb_node_temp; /* Point to the next node */
+    }
     
     
     return 0;
