b25deee3f582dd2de05e1281fa4f3a23d0c2ec1d
Tamaarine <irebootplaygt@gmail.com>
2022-10-05 15:28:02 +0000

    Added Makefile and source file for part 1b, just getting started with port parsing

---
+++ HW3/s2dsm_P1b.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int main(int argc, char ** argv) {
+    if (argc != 3) {
+        printf("You will need to specify 2 arguments!\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    /* Parse the first number and second number */
+    errno = 0;
+    int local_port = strtol(argv[1], NULL, 0);
+    if (errno) {
+        printf("Converting number failed\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    errno = 0;
+    int remote_port = strtol(argv[2], NULL, 0);
+    if (errno) {
+        printf("Converting number failed\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("The number returned %d %d\n", local_port, remote_port);
+    
+    return 0;
+}
