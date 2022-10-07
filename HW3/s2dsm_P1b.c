f1cc7f961f64fc26b8698e241257553b70536e7c
Tamaarine <irebootplaygt@gmail.com>
2022-10-07 20:15:52 +0000

    Changed the logic of P1B, now it will be creating a thread to handle connection, the main thread will be handling sending the messages

--- HW3/s2dsm_P1b.c
+++ HW3/s2dsm_P1b.c
@@ -6,21 +6,72 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <pthread.h>
+#include <string.h>
+#include <fcntl.h>
 
-void server_logic(int fd) {
-    
-}
+#define MAX_SIZE 50
 
-void client_logic(int fd) {
+static void * server_thread(void * arg) {
+    /* This server thread will be accepting the connections */
+    struct sockaddr_in address; /* Used for setting up the server accept socket */
+    int server_port = *((int *)arg); /* Get the port that's passed into the thread */
+    int sockfd; /* Socket used for listening */
+    int opt = 1; /* Enable boolean options */
+    int accepted_socket; /* Socket that's accepted */
+    int address_len = sizeof(address);
     
+    printf("I'm the server thread and im going to listen on %d\n", server_port);
+    
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation error");
+        exit(EXIT_FAILURE);
+    }
+    
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                &opt, sizeof(opt))) {
+        perror("Setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(server_port);
+    
+    /*Bind the sockfd to the address struct  */
+    if (bind(sockfd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    if (listen(sockfd, 3)) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    /* Wait for connection */
+    if ((accepted_socket = accept(sockfd, (struct sockaddr *)&address, 
+            (socklen_t *)&address_len)) < 0) {
+        perror("Accept failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("Accepted connection\n");
+        
+    pthread_exit(NULL);
 }
 
 int main(int argc, char ** argv) {
-    int connect_socket; /* Used for creating the two way socket connection */
-	struct sockaddr_in address; /* Used for configuring the socket */
-	struct sockaddr_in address_out; /* Used for connecting to the other socket */
-    int opt = 1; /* Used in setsocketopt to enable boolean option */
-    int address_len = sizeof(address);
+    int connect_socket; /* Used for connecting to the other process */
+    struct sockaddr_in address_out;
+    // char * inputBuf; /* Used for storing the user input */
+    // int address_len = sizeof(address);
+    
+    /* Used for creating the thread.
+     * The thread acts as the server to accept incoming connections
+     * The main thread will act as the message sender
+     */
+    pthread_t thread_id;
     
     if (argc != 3) {
         printf("You will need to specify 2 arguments!\n");
@@ -29,101 +80,49 @@ int main(int argc, char ** argv) {
     
     /* Parse the first number and second number */
     errno = 0;
-    int listen_port = strtol(argv[1], NULL, 0);
+    int * listen_port = malloc(sizeof(int));
+    *listen_port = strtol(argv[1], NULL, 0);
     if (errno) {
-        printf("Converting number failed\n");
+        perror("Converting number failed");
         exit(EXIT_FAILURE);
     }
     
     errno = 0;
-    int send_port = strtol(argv[2], NULL, 0);
+    int * send_port = malloc(sizeof(int));
+    *send_port = strtol(argv[2], NULL, 0);
     if (errno) {
-        printf("Converting number failed\n");
+        perror("Converting number failed");
         exit(EXIT_FAILURE);
     }
     
-    printf("Listening port is %d sending port is %d\n", listen_port, send_port);
+    printf("Listening port is %d sending port is %d\n", *listen_port, *send_port);
     
-    /* Create the two way socket that we will be using
-     * The communication domain will be AF_INET. It will be connecting
-     * to the same network reaching a specified process port for communication
-     */
-    if ((connect_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("Socket creation error\n");
-        exit(EXIT_FAILURE);
-    }
+    /* Need to spin up a thread to handle the connection */    
+    pthread_create(&thread_id, NULL, server_thread, (void *) listen_port);
     
-    /* Socket option setting to allow reuse of address and reuse of port */
-    if (setsockopt(connect_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                &opt, sizeof(opt))) {
-        printf("Setsocket failed\n");
-        exit(EXIT_FAILURE);
-    }
+    // inputBuf = malloc(sizeof(char) * MAX_SIZE);
     
-    /* address struct for the current socket.
-     * Listen on all network interface for connection
-     * The current socket is assigned the listening_port.
-     */
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(listen_port);
+    // inputBuf = fgets(inputBuf, MAX_SIZE, stdin);
     
-    /*Bind the connect_socket to the address struct  */
-    if (bind(connect_socket, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        printf("Bind failed %d\n", errno);
-        exit(EXIT_FAILURE);
-    }
+    // printf("The user entered -%s-\n", inputBuf);
     
-    /* Configures the address that this socket is connecting to */
+    connect_socket = socket(AF_INET, SOCK_STREAM, 0);
+    
     address_out.sin_family = AF_INET;
-    address_out.sin_port = htons(send_port);
+    address_out.sin_port = htons(*send_port);
     
-    if (inet_pton(AF_INET, "127.0.0.1", &address_out.sin_addr) <= 0) {
-        printf("Invalid address or error from inet_pton\n");
-        exit(EXIT_FAILURE);
+    if(inet_pton(AF_INET, "127.0.0.1", &address_out.sin_addr) <= 0) {
+		perror("inet_pton failed");
+		exit(EXIT_FAILURE);
+	}
+    
+    /* Client connect here to the other port */
+    while(connect(connect_socket, (struct sockaddr *)&address_out, sizeof(address_out)) < 0) {
+        // perror("Connection failed retrying");
+        sleep(1);
     }
     
-    /* If this connect failed then that means the server doesn't exist
-     * Then the first instance of this program will be declared as the server
-     */
-    if (connect(connect_socket, (struct sockaddr *)&address_out, sizeof(address_out)) < 0) {
-        /* Make sure it error out due to server not existing */
-        if (errno == ECONNREFUSED) {
-            /* Reach here means server, set connect_socekt as server fd */
-            if (listen(connect_socket, 3)) {
-                printf("Listen failed\n");
-                exit(EXIT_FAILURE);
-            }
-            
-            int accepted_socket;
-            
-            /* Wait for connection */
-            if ((accepted_socket = accept(connect_socket, (struct sockaddr *)&address, 
-                    (socklen_t *)&address_len)) < 0) {
-                printf("Accept failed %d\n", errno);
-            }
-            
-            printf("Accepted connection\n");
-            server_logic(accepted_socket);
-            
-            /* Close both the accepted socket and server socket */
-            close(connect_socket);
-            close(accepted_socket);
-        }
-        else {
-            /* If other error then exit program */
-            printf("Connection failed %d\n", errno);
-            exit(EXIT_FAILURE);
-        }
-        
-    }
-    else {
-        printf("Connected sucessfully to server!\n");
-        client_logic(connect_socket);
-        
-        /* Close the socket */
-        close(connect_socket);
-    }
+    pthread_join(thread_id, NULL);
     
     return 0;
 }
